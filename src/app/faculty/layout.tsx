c693df5eacc2ae313ead334d6a33b66eb4e1c1d3
JAE HEUNG PARK <56800332+renaissancor@users.noreply.github.com>
2024-08-06 23:12:19 +0800

    feat: added little title for sponsors and faculty layout

--- src/app/faculty/layout.tsx
+++ src/app/faculty/layout.tsx
@@ -1,7 +1,19 @@
+import { Separator } from "@/components/ui/separator";
+import { Title } from "@radix-ui/react-toast";
+
 export default function FacultyLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Title className="m-4 text-bf">
+        Faculty Members of University of Michigan-Shanghai Jiao Tong University
+        Joint Institute
+      </Title>
+      <Separator />
+      <main>{children}</main>
+    </div>
+  );
 }
--- src/app/sponsors/layout.tsx
+++ src/app/sponsors/layout.tsx
@@ -1,13 +1,19 @@
-import { sponsors } from "@/data/sponsors";
+import { Separator } from "@/components/ui/separator";
+import { Title } from "@radix-ui/react-toast";
 import { ReactNode } from "react";
 
 type ProjectIDLayoutProps = {
   children: ReactNode;
 };
 
-export default function FacultyIDLayout({ children }: ProjectIDLayoutProps) {
+export default function SponsorsLayout({ children }: ProjectIDLayoutProps) {
   return (
     <div>
+      <Title className="m-4 text-bf">
+        Sponsors of Capstone Design projects of University of Michigan-Shanghai
+        Jiao Tong University Joint Institute
+      </Title>
+      <Separator />
       <main>{children}</main>
     </div>
   );
