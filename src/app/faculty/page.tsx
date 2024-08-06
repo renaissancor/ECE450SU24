3bec7b3702d0f2fadb97e77c0d06ec3cb8a09a55
JAE HEUNG PARK <56800332+renaissancor@users.noreply.github.com>
2024-08-06 21:47:11 +0800

    add: faculty ui enhance

--- src/app/faculty/page.tsx
+++ src/app/faculty/page.tsx
@@ -7,28 +7,36 @@ import {
   CardFooter,
 } from "@/components/ui/card";
 import Link from "next/link";
-import { instructors } from "@/data/faculty"; // Adjust the import path based on your actual file structure
-import { Instructor } from "@/data/faculty";
+// import { instructors } from "@/data/faculty"; // Adjust the import path based on your actual file structure
+// import { Instructor } from "@/data/faculty";
 import { Button } from "@/components/ui/button";
 // Assuming your faculty data is exported as an array of Instructor objects
+import { Professor, professors } from "./data";
 
-const FacultyMember = ({ instructor }: { instructor: Instructor }) => {
+const FacultyMember = ({ professor }: { professor: Professor }) => {
   return (
     <Card className="faculty-member p-4 flex flex-col h-full">
       <CardHeader className="flex flex-col items-center">
         <Image
-          src={`/faculty/${instructor.id}.jpg`}
-          alt={instructor.name}
+          src={`/faculty/${professor.name.replace(" ", "_").toLowerCase()}.jpg`}
+          alt={professor.name}
           width={150}
           height={150}
           className="rounded-full"
         />
       </CardHeader>
       <CardContent className="flex-grow">
-        <CardTitle className="text-center mt-4">{`${instructor.name}`}</CardTitle>
+        <CardTitle className="text-center mt-4">Dr. {professor.name}</CardTitle>
+        <p className="text-center">{professor.title}</p>
+        <p className="text-center">Office : {professor.office}</p>
+        <p className="text-center">{professor.telephone}</p>
+        {/*  <p className="text-center">{professor.email}</p> */}
       </CardContent>
       <CardFooter>
-        <Link className="w-full" href={`/faculty/${instructor.id}`}>
+        <Link
+          className="w-full"
+          href={`/faculty/${professor.name.replace(" ", "_").toLowerCase()}`}
+        >
           <Button className="w-full">View Profile</Button>
         </Link>
       </CardFooter>
@@ -39,9 +47,9 @@ const FacultyMember = ({ instructor }: { instructor: Instructor }) => {
 export default function FacultyPage() {
   return (
     <div className="m-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {instructors.map((instructor) => (
-          <FacultyMember key={instructor.id} instructor={instructor} />
+      <div className="grid grid-cols-3 gap-4">
+        {professors.map((professor, index) => (
+          <FacultyMember key={index} professor={professor} />
         ))}
       </div>
     </div>
